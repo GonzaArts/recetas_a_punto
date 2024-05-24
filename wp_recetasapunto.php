dd81c9d87df647b369815bfe0490549543b4ded1
GonzaArts <gonzagamer79@gmail.com>
2024-05-24 03:38:17 +0200

    :bug: Arreglado el problema con la muestra del css y js en el frontend

--- wp_recetasapunto.php
+++ wp_recetasapunto.php
@@ -22,22 +22,19 @@ require_once plugin_dir_path(__FILE__) . 'includes/shortcode.php';
 // Admin Metabox
 if (is_admin()) {
     require_once plugin_dir_path(__FILE__) . 'admin/recipe-metabox.php';
-    require_once plugin_dir_path(__FILE__) . 'admin/shortcode-metabox.php';     
+    require_once plugin_dir_path(__FILE__) . 'admin/shortcode-metabox.php';
 }
 
-function wp_recetasapunto_enqueue_scripts() {
-    wp_enqueue_style('wp-recetasapunto-admin-style', plugins_url('assets/css/admin-style.css', __FILE__));
-    wp_enqueue_script('wp-recetasapunto-admin-script', plugins_url('assets/js/admin-script.js', __FILE__), array('jquery'), null, true);
+// Enqueue admin styles and scripts
+function wp_recetasapunto_enqueue_admin_scripts($hook_suffix) {
+    wp_enqueue_style('wp-recetasapunto-admin-style', plugin_dir_url(__FILE__) . 'assets/css/admin-style.css');
+    wp_enqueue_script('wp-recetasapunto-admin-script', plugin_dir_url(__FILE__) . 'assets/js/admin-script.js', array('jquery'), null, true);
 }
-add_action('admin_enqueue_scripts', 'wp_recetasapunto_enqueue_scripts');
+add_action('admin_enqueue_scripts', 'wp_recetasapunto_enqueue_admin_scripts');
 
-function wp_recetasapunto_enqueue_frontend_scripts() {
-    wp_enqueue_script('wp-recetasapunto-frontend-script', plugins_url('assets/js/frontend-script.js', __FILE__), array('jquery'), null, true);
+// Enqueue frontend styles and scripts
+function wp_recetasapunto_enqueue_frontend_assets() {
+    wp_enqueue_style('wp_recetasapunto-frontend-style', plugin_dir_url(__FILE__) . 'assets/css/frontend-style.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('wp_recetasapunto-frontend-script', plugin_dir_url(__FILE__) . 'assets/js/frontend-script.js', array('jquery'), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', 'wp_recetasapunto_enqueue_frontend_scripts');
-
-function wp_recetasapunto_enqueue_frontend_styles() {
-    wp_enqueue_style('wp-recetasapunto-frontend-style', plugins_url('assets/css/frontend-style.css', __FILE__));
-}
-add_action('wp_enqueue_scripts', 'wp_recetasapunto_enqueue_frontend_styles');
-
+add_action('wp_enqueue_scripts', 'wp_recetasapunto_enqueue_frontend_assets');
