e3df2158301ef32ca1d25805308500f3624a0e0d
GonzaArts <gonzagamer79@gmail.com>
2024-09-02 00:27:30 +0200

    :sparkles: Añadido nuevos idiomas

--- includes/i18n.php
+++ includes/i18n.php
@@ -1,5 +1,5 @@
 <?php
 function wp_recetasapunto_load_textdomain() {
-    load_plugin_textdomain('wp_recetasapunto', false, dirname(plugin_basename(__FILE__)) . '/../languages');
+    load_plugin_textdomain('wp_recetasapunto', false, dirname(plugin_basename(__FILE__)) . '/languages');
 }
 add_action('plugins_loaded', 'wp_recetasapunto_load_textdomain');
